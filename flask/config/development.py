6e4a170561cb035b8be9dffce03dcece18651e9c
Alexander Belugin <alexander.belugin@outlook.com>
2016-09-13 23:58:15 +0300

    Refactore existing Flask code.

--- flask/config/development.py
+++ flask/config/development.py
@@ -1,6 +1,3 @@
 import os
 
 DEBUG = True
-SECRET_KEY = 'top secret!'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.dirname(__file__), 'data.sqlite3')
-SQLALCHEMY_TRACK_MODIFICATIONS = False

0810a1c344d021d2ab427143defa0d91e04306f8
Alexander Belugin <alexander.belugin@outlook.com>
2016-09-14 23:11:36 +0300

    Refactor JS extension.

--- extension/feedly.js
+++ extension/feedly.js
@@ -1,94 +1,55 @@
-// var $myjq;
-// function getScript(){
-//     var script=document.createElement('script');
-//     script.src='jquery_my.js';
-//     var head=document.getElementsByTagName('head')[0],
-//     done=false;
-//     // Attach handlers for all browsers
-//     script.onload=script.onreadystatechange = function(){
-//         if ( !done && (!this.readyState
-//             || this.readyState == 'loaded'
-//             || this.readyState == 'complete') ) {
-//             done=true;
-//             $myjq=jQuery.noConflict();
-//             script.onload = script.onreadystatechange = null;
-//         }
-//     };
-//     head.appendChild(script);
-// }
-/*
-title is here:
-<a href="http://www.cnews.ru/news/line/2016-06-27_bolee_2_mln_rossiyan_zaregistrirovalis_na_portale" id="tc/ww8EU3snQHOXN7MKc9qd83wTQVsk77s9vXdX0Ct0=_15591e68863:48fe4d4:85ca5f35_entry_title" class="entryTitle title read" target="_blank" cdf_container="10">Более&nbsp;2 млн россиян зарегистрировались на портале «Почты России»</a>
+var service_host = 'http://localhost:5000/store/'
 
-short summary:
-<div class="content" itemprop="description" id="tc/ww8EU3snQHOXN7MKc9qd83wTQVsk77s9vXdX0Ct0=_15591e68863:48fe4d4:85ca5f35_entryContent" cdf_container="3"> 
-На интернет-портале pochta.ru более 2 млн пользователей завели личные кабинеты, синхронизированные с мобильным п 
-</div>
-
-url is inside the positive link:
-
-/*
-
-Send positive example when clicking this link:
-<a href="http://www.cnews.ru/news/line/2016-06-27_bolee_2_mln_rossiyan_zaregistrirovalis_na_portale" class="fx-button secondary full-width" style="margin-top:24px" target="_blank"> 
-Visit Website 
-</a>
-*/
-
-/*
-Send negative example when clicking this link:
-<img data-buryentryid="tc/ww8EU3snQHOXN7MKc9qd83wTQVsk77s9vXdX0Ct0=_15591e68863:48fe4d5:85ca5f35" src="images/condensed-close-black.png" title="Mark as read and hide">
-*/
+function send_example(label, url, date, title, summary) {
+	console.log(label + ': ' + url);
+	request_url = service_host + '?';
+	request_url += 'label=' + label;
+	request_url += '&url=' + encodeURIComponent(url);
+	request_url += '&date=' + encodeURIComponent(date);
+	request_url += '&title=' + encodeURIComponent(title);
+	request_url += '&summary=' + encodeURIComponent(summary);
+	$.get(request_url);
+}
 
 function positive() {
-	//alert('That was good!');
-	console.log('good');
 	url = this.getAttribute('href');
 	date = new Date().toLocaleString();
-	console.log(url);
-	$.get('http://localhost:5000/store/good?url=' + encodeURIComponent(url) + '&date=' + encodeURIComponent(date));
+	title = '';
+	summary = '';
+	send_example('good', url, date, title, summary);
 }
 function negative() {
-	//alert('That was bad!');
-	console.log('bad');
 	url = $(this).parent('.condensedTools').children('a').prop('href');
 	date = new Date().toLocaleString();
 	title = $(this).parent('.condensedTools').parent('div').children('div:last').children('div:last').children('a').text();
 	summary = $(this).parent('.condensedTools').parent('div').children('div:last').children('div:last').children('span').text();
-	
-	console.log(url);
-	$.get('http://localhost:5000/store/bad?url=' + encodeURIComponent(url) + '&date=' + encodeURIComponent(date) + '&title=' + encodeURIComponent(title) + '&summary=' + encodeURIComponent(summary));
+	send_example('bad', url, date, title, summary);
 }
+
+function setPositive() {
+	$(document).on('click', '.fx-button.full-width', positive);
+	console.log('Positive trigger set.');
+}
+
+function setNegative() {
+	$(document).on('click', '.condensedTools img', negative);
+	console.log('Negative trigger set.');
+}
+
+function doStuff() {
+	setPositive();
+	setNegative();
+	console.log('Triggers set.');
+}
+
 function waitForFeedLoad() {
     if ($('.u0Entry').length == 0) {
         setTimeout(waitForFeedLoad,50);
     }
     else {
-    	console.log('feed loaded.');
+    	console.log('Feed loaded.');
     	doStuff();
     }
-}
-function setPositive() {
-	$(document).on('click', '.fx-button.full-width', positive);
-	console.log('positive set.');
-}
-function setNegative() {
-	$(document).on('click', '.condensedTools img', negative);
-	// $('.condensedTools img').off('click', negative);
-	// $('.condensedTools img').click( negative );
-	console.log('negative set.');
-	//setTimeout(setNegative, 500);
-}
-function setPositiveDelayed() {
-	setTimeout(setPositive, 500);
-}
-function doStuff() {
-	setPositive();
-	//$('.title').click( setPositiveDelayed );
-	setNegative();
-	// $myjq('.fx-button.full-width').live('click', positive );
-	// $myjq('.condensedTools img').live('click', negative );
-	console.log('stuff done.');
 }
 
 waitForFeedLoad();
